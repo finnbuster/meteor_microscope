780e2fd49ce82c8c81c476866030c494e3810020
Finn Robertson <finnbuster@gmail.com>
2015-08-13 16:51:06 +1000

    Removed auto publish and published and subscribed to posts

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,18 +1,6 @@
-var postsData = [
-  {
-    title: 'Introducing Telescope',
-    url: 'http://sachagreif.com/introducing-telescope/'
-  }, 
-  {
-    title: 'Meteor',
-    url: 'http://meteor.com',
-    cool: true
-  }, 
-  {
-    title: 'The Meteor Book',
-    url: 'http://themeteorbook.com'
-  }
-];
+
 Template.postsList.helpers({
-  posts: postsData
+  posts: function () {
+    return Posts.find();
+  }
 });
